e225ee95c8b97f3d36581b314c39e041bea6591d
Scalabrini17 <itallomartins2007@gmail.com>
2026-04-16 15:06:01 -0300

    Update Exercises

--- At8Aula13/ex03.java
+++ At8Aula13/ex03.java
@@ -4,7 +4,18 @@ public class ex03 {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
 
-        
+        float soma = 0;
 
+        for (int i = 1; i <= 7; i++){
+            System.out.printf("Digite a %dº venda: ", i);
+            float num = sc.nextFloat(); 
+            soma += num;
+        }
+
+        float media = soma / 7;
+
+        System.out.printf("A soma entre as sete vendas da semana é: R$%.2f\n", soma);
+
+        System.out.printf("A média entre as sete vendas da semana é: R$%.2f\n", media);
     }
 }
