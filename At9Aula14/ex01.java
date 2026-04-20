191758d0582f44dde80b09da386ed8e422afff92
Scalabrini17 <itallomartins2007@gmail.com>
2026-04-20 08:54:51 -0300

    Starting exercise At9

---
+++ At9Aula14/ex01.java
@@ -0,0 +1,13 @@
+public class ex01{
+    public static void main(String[] args) {
+        
+    }
+}
+
+// Em uma das análises, é necessário listar apenas os valores
+// pares, pois eles representam eventos que atendem a um determinado critério técnico
+// do sistema.
+// Com base nesse contexto, desenvolva um algoritmo em Java que leia dois
+// números inteiros A e B (sendo A < B) e, utilizando a estrutura de repetição for, exiba
+// todos os números pares existentes no intervalo entre A e B, incluindo os próprios limites
+// caso também sejam pares.
