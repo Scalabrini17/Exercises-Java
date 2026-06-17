20c530c036f5885714fafc15bd96706ed270e65b
Scalabrini17 <itallomartins2007@gmail.com>
2026-06-17 10:39:13 -0300

    Update: Finalizado At18Aula33

--- At18Aula33/SGNAC.java
+++ At18Aula33/SGNAC.java
@@ -5,7 +5,7 @@ public class SGNAC {
     public static Scanner sc = new Scanner(System.in);
 
     // Dados
-    public static double[] notas = new double[4];
+    public static double[] notas = new double[5];
     public static double somaNotas = 0;
     public static double mediaNotas = 0;
     public static String nome = "";
@@ -15,60 +15,65 @@ public class SGNAC {
 
         System.out.println("Bem vindo ao Sistema de Gerenciamento de Notas Academicas!");
         System.out.println("Vamos iniciar!");
-        lernotas();
+         System.out.println();
+        NomeAluno();
+        lernotas(); 
         calcularMedia();
         determinarSituacao();
         exibirRelaatorio();
+    
     }
 
-    public static void lernotas(){
+    static void NomeAluno(){
 
         System.out.print("Digite o nome do aluno: ");
         nome = sc.next();
+        System.out.println();
 
-        System.out.printf("Insira as 4 notas do aluno %s \n", nome);
-        for(int i = 0; i < 4; i++){
-            System.out.printf("Insira a %dº nota: ", i);
-            notas[i] = sc.nextDouble();
-        }
+    }
+
+    static void lernotas(){
+
+       for(int i = 1; i < notas.length; i++){
+        System.out.printf("Digite a %dº nota: ", i);
+        notas[i] = sc.nextDouble();
+       }
 
     }
 
     public static void calcularMedia(){
 
-        for(int i = 0; i < 4; i++){
-            if(notas[i] != 0){
-                notas[i] += somaNotas;
-            }
+        for(int i = 1; i < notas.length; i++){
+             somaNotas += notas[i];
         }
 
-        mediaNotas += (somaNotas / notas.length);
+        mediaNotas = somaNotas / 4;
 
     }
 
     public static void determinarSituacao(){
 
         if(mediaNotas >= 7){
-            situacao += "APROVADO!";
+            situacao = "APROVADO!";
 
         }else if(mediaNotas >= 4 && mediaNotas <= 6.9){
-            situacao += "EM RECUPERAÇÃO!";
+            situacao = "EM RECUPERAÇÃO!";
 
         }else if(mediaNotas < 4){
-            situacao += "REPROVADO!";
+            situacao = "REPROVADO!";
         }
 
     }
 
     public static void exibirRelaatorio(){
-
+        System.out.println();
         System.out.println("===== Relatório Final =====");
         System.out.printf("Nome do aluno: %s \n",nome);
-        System.out.printf("Notas: %.2f %.2f %.2f %.2f \n", notas[0], notas[1], notas[2], notas[3]);
-        System.out.printf("Média das notas: %.2f \n", mediaNotas);
+        System.out.printf("Notas: %.1f %.1f %.1f %.1f \n", notas[1], notas[2], notas[3], notas[4]);
+        System.out.printf("Média das notas: %.1f \n", mediaNotas);
         System.out.printf("Situação: %s \n", situacao);
         System.out.print("===========================");
-
+        
 
     }
 
